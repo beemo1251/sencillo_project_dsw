1afa56fc907f8cd4e6008fc3bff740e871eb8f53
angeloleon <elangelusvlr@gmail.com>
2021-05-21 11:56:29 -0500

    Vistas de Index y Login

--- Solucion_Sencillo_Project_DSW/Sencillo_Project_DSW/Controllers/HomeController.cs
+++ Solucion_Sencillo_Project_DSW/Sencillo_Project_DSW/Controllers/HomeController.cs
@@ -8,22 +8,14 @@ namespace Sencillo_Project_DSW.Controllers
 {
     public class HomeController : Controller
     {
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
